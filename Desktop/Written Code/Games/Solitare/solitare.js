c91ad2037ec06a5d862404e8bdc25dc7c246d9de
Chase Ward <chasew8095@gmail.com>
2023-05-30 23:33:12 -0500

    better version

--- Desktop/Written Code/Games/Solitare/solitare.js
+++ Desktop/Written Code/Games/Solitare/solitare.js
@@ -1,95 +1,27 @@
 // shuffle
-let cards = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
-    let currentIndex = cards.length,  randomIndex;
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [cards[currentIndex], cards[randomIndex]] = [
-        cards[randomIndex], cards[currentIndex]];
-    }
-// sort through all bottom cards
-for (i=0; i<7; i++) {
-    // all bottom cards on the tableau
-    switch (i) {
-        case 0:
-            spot = 'r2c1'
-            break
-        case 1:
-            spot = 'r3c2'
-            break
-        case 2:
-            spot = 'r4c3'
-            break
-        case 3:
-            spot = 'r5c4'
-            break
-        case 4:
-            spot = 'r6c5'
-            break
-        case 5:
-            spot = 'r7c6'
-            break
-        case 6:
-            spot = 'r8c7'
-            break
-    }
-    // put on tableau
-    switch (cards[i]) {
-        case "ace":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Ace.png)'
-            cards.splice(cards.indexOf('ace'), 1)
-            break
-        case "two":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Two.png)'
-            cards.splice(cards.indexOf('two'), 1)
-            break
-        case "three":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Three.png)'
-            cards.splice(cards.indexOf('three'), 1)
-            break
-        case "four":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Four.png)'
-            cards.splice(cards.indexOf('four'), 1)
-            break
-        case "five":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Five.png)'
-            cards.splice(cards.indexOf('five'), 1)
-            break
-        case "six":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Six.png)'
-            cards.splice(cards.indexOf('six'), 1)
-            break
-        case "seven":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Seven.png)'
-            cards.splice(cards.indexOf('seven'), 1)
-            break
-        case "eight":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Eight.png)'
-            cards.splice(cards.indexOf('eight'), 1)
-            break
-        case "nine":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Nine.png)'
-            cards.splice(cards.indexOf('nine'), 1)
-            break
-        case "ten":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Ten.png)'
-            cards.splice(cards.indexOf('ten'), 1)
-            break
-        case "jack":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Jack.png)'
-            cards.splice(cards.indexOf('jack'), 1)
-            break
-        case "queen":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/Queen.png)'
-            cards.splice(cards.indexOf('queen'), 1)
-            break
-        case "king":
-            document.getElementById(spot).style.backgroundImage = 'url(Diamonds/King.png)'
-            cards.splice(cards.indexOf('king'), 1)
-            break
-    }
-    //make cards "fit" to shape
-    document.getElementById(spot).style.backgroundSize = '100%'
-    document.getElementById(spot).style.height = '102%'
-    document.getElementById(spot).style.backgroundRepeat = 'no-repeat'
+let deck = [
+    // clubs
+    ['ace', 'clubs'], ['two', 'clubs'], ['three', 'clubs'], ['four', 'clubs'], ['five', 'clubs'], ['six', 'clubs'], ['seven', 'clubs'], ['eight', 'clubs'], ['nine', 'clubs'], ['ten', 'clubs'], ['jack', 'clubs'], ['queen', 'clubs'], ['king', 'clubs'],
+    // spades
+    ['ace', 'spades'], ['two', 'spades'], ['three', 'spades'], ['four', 'spades'], ['five', 'spades'], ['six', 'spades'], ['seven', 'spades'], ['eight', 'spades'], ['nine', 'spades'], ['ten', 'spades'], ['jack', 'spades'], ['queen', 'spades'], ['king', 'spades'],
+    // diamonds
+    ['ace', 'diamonds'], ['two', 'diamonds'], ['three', 'diamonds'], ['four', 'diamonds'], ['five', 'diamonds'], ['six', 'diamonds'], ['seven', 'diamonds'], ['eight', 'diamonds'], ['nine', 'diamonds'], ['ten', 'diamonds'], ['jack', 'diamonds'], ['queen', 'diamonds'], ['king', 'diamonds'],
+    // hearts
+    ['ace', 'hearts'], ['two', 'hearts'], ['three', 'hearts'], ['four', 'hearts'], ['five', 'hearts'], ['six', 'hearts'], ['seven', 'hearts'], ['eight', 'hearts'], ['nine', 'hearts'], ['ten', 'hearts'], ['jack', 'hearts'], ['queen', 'hearts'], ['king', 'hearts']
+]
+
+// shuffle
+let currentIndex = deck.length,  randomIndex;
+while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [deck[currentIndex], deck[randomIndex]] = [
+    deck[randomIndex], deck[currentIndex]];
 }
+
+// pre-game set tableau 
+const cardClass = document.querySelectorAll('.card').forEach(card => {
+    card.style.backgroundImage = 'url(/'+deck[0][1]+'/'+deck[0][0]+'.png)'
+    card.style.backgroundRepeat = 'no-repeat'
+    deck.splice(0, 1)
+})
